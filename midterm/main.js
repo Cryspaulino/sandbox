8d91609dd96dce8146d4370102eca4174a02101f
Cryspaulino <[Cryspaulino]@users.noreply.github.com>
2026-06-23 20:35:32 -0600

    Changes I had pending.

--- midterm/main.js
+++ midterm/main.js
@@ -1,18 +1,66 @@
-const results = document.getElementById('results');
-const container = document.getElementById('container');
+const resultslist = document.getElementById('results');
+const output = document.getElementById('container');
 
-function matchSearch() {
-    return ;
+const url = "https://api.inaturalist.org/v1/identifications"
+const urllist = "https://api.inaturalist.org/v1/places"
+
+let results = null;
+
+// function matchSearch() {
+//     return ;
+// }
+
+async function getPlace(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        displayCards(data);
+    }
 }
 
-function displayList() {
-    return ;
+function displayCards(data) {
+    results = data;
+
+    const template = `<h1>Place: ${data.name} </h1>`
+    
+    output.innerHTML = template;
 }
 
-function selectedResults() {
-    return ;
+
+function displayList(data) {
+    console.log(data)
+
+    resultslist.innerHTML = '';
+    
+    resultslist.forEach((place) => {
+        const listtemplate = `<li> <button data-url="${place.url}"> ${place.display_name} </button> </li> `;
+        
+        resultslist.innerHTML += listtemplate;
+    });
+
+    const buttons = resultslist.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            getPlace(button.dataset.url);
+        });
+    });
 }
 
-function displayCards() {
-    return ;
+async function getPlaceList(url) {
+    const listresponse = await fetch(url);
+    if (listresponse.ok) {
+        const data = await listresponse.json();
+        displayList(data);
+    };
 }
+
+getPlace(url);
+getPlaceList(urllist);
+
+// function selectedResults() {
+//     return ;
+// }
+
+
+
